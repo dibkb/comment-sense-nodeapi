be566edd3114e121ae954739cd675550ed302fd7
dibkb <dibas9110@gmail.com>
2024-07-08 10:36:39 +0530

    search

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.get("/get-info/:ytid", async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
+app.get("/search-video/:title", async (req, res) => {
+  try {
+    const { title } = req.params;
+    if (!title) {
+      return res.status(400).json({ message: "No title found" });
+    }
+    const data = await ytext.search(title);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 const port = process.env.PORT || 9000;
 
