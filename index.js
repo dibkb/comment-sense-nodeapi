2997369c89d18d7f8c44006fee617f4145238092
dibkb <dibas9110@gmail.com>
2024-08-24 23:35:38 +0530

    converter

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import ytext from "youtube-ext";
-
+import { Client } from "youtubei";
+import { convertToVideoInfo } from "./convert.js";
+const youtube = new Client();
 const app = express();
 
 // Middleware
@@ -20,8 +22,9 @@ app.get("/get-info/:ytid", async (req, res) => {
     if (!ytid) {
       return res.status(400).json({ message: "No youtube id provided" });
     }
-    const data = await ytext.videoInfo(ytid);
-    return res.status(200).json(data);
+    const data = await youtube.getVideo(ytid);
+    console.log(convertToVideoInfo(data));
+    return res.status(200).json(convertToVideoInfo(data));
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
