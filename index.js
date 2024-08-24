ff27f0b20297ad2ef087a78f60a8bd49f3c5248a
dibkb <dibas9110@gmail.com>
2024-08-25 00:05:06 +0530

    new branch

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import ytext from "youtube-ext";
-import { Client } from "youtubei";
-import { convertToVideoInfo } from "./convert.js";
-const youtube = new Client();
 const app = express();
 
 // Middleware
@@ -19,12 +16,11 @@ app.get("/test", (req, res) => {
 app.get("/get-info/:ytid", async (req, res) => {
   try {
     const { ytid } = req.params;
-    console.log(ytid);
     if (!ytid) {
       return res.status(400).json({ message: "No youtube id provided" });
     }
-    const data = await youtube.getVideo(ytid);
-    return res.status(200).json(convertToVideoInfo(data));
+    const data = await ytext.videoInfo(ytid);
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
